4da002606f5d2b533fb99f368ef9d14662e56c20
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-05-12 16:29:15 -0700

    create Article ListComponent

--- src/components/ArticleList/ArticleList.jsx
+++ src/components/ArticleList/ArticleList.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Article from '../Article/Article'
 
-function ArticleList(props) {
+function ArticleList({articles}) {
   return (
     <div>
-      ArticleList
+      <ul aria-label="article-list">
+        {articles.map(article => <Article {...article}/>)}
+      </ul>
     </div>
   )
 }
 
 ArticleList.propTypes = {
-
+  articles: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }))
 }
 
 export default ArticleList
